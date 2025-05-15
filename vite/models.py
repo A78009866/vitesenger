bc7aabfdb2c9cbd4f0fa0f5527c908bedda306e3
A78009866 <salemdopamine@gmail.com>
2025-05-15 14:32:11 +0100

    g

--- vite/models.py
+++ vite/models.py
@@ -16,7 +16,6 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"@{self.username}"
-    
 
 class Message(models.Model):
     sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sent_messages")
