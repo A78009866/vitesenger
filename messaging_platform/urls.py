0747d895b686969576dab0bab727c95152b241f9
A78009866 <salemdopamine@gmail.com>
2025-08-11 22:05:53 +0100

    g

--- messaging_platform/urls.py
+++ messaging_platform/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.splash, name='splash'),
     path('', include('vite.urls')),
-    path('home/', views.home, name='home'),
+    path('home/', views.chat_list, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='social/login.html'), name='login'),
     path('vite/', include('vite.urls')),
 ]
